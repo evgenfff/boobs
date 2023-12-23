ac98ebdd584847f2645787ee4ffa3ed0b54d3b6f
Evgeny Novikov <68502931+evgenfff@users.noreply.github.com>
2023-12-24 00:40:25 +0600

    Update goodbyes.py

--- examples/nipples/goodbyes.py
+++ examples/nipples/goodbyes.py
@@ -1,4 +1,4 @@
-from boobs import Nipple, Message
+from boobs import Nipple, Cock
 from boobs.bot.rules import Text
 
 # Make a nipple of a bot.
@@ -6,10 +6,10 @@ nip = Nipple()
 
 
 @nip.on.message(Text(["bye", "cheers"], ignore_case=True))
-async def bye_handler(m: Message) -> None:
-    await m.answer("See you soon!")
+async def bye_handler(c: Cock) -> None:
+    await c.cum("See you soon!")
 
 
 @nip.on.message(Text(["take care", "have a good day"], ignore_case=True))
-async def goodbye_handler(m: Message) -> None:
-    await m.answer("You too, bye!")
+async def goodbye_handler(c: Cock) -> None:
+    await c.cum("You too, bye!")
